ac3dd6d85ac4b992758da630f54cb8ad3242a1c9
marcio <sistemarcio@gmail.com>
2015-05-27 02:15:31 -0300

    beta

--- js/jr.js
+++ js/jr.js
@@ -15,7 +15,7 @@ function reader(){
 
 document.getElementById('header').innerHTML = ""
 +
-"<header id='site'>"
+"<header id='myreader'>"
 +
 "<a href='http://tentthenet.org' id='index' class='link' title='http://tentthenet.org'>"
 +
@@ -28,17 +28,18 @@ document.getElementById('header').innerHTML = ""
 "<iframe id='logo' scrolling='no' src='img/tentthenet_alfa.png'></iframe>"
 +
 "";
-document.getElementById('site').style.fontFamily= "Sawasdee";
-document.getElementById('site').style.color= "blue";
-document.getElementById('site').style.backgroundColor= "#E3F1EC";
-document.getElementById('site').style.backgroundImage= "url(css/fundo2.png)";
-document.getElementById('site').style.textDecoration= "underline";
-document.getElementById('site').style.textAlign= "center";
+document.getElementById('index').style.fontFamily= "Sawasdee";
+document.getElementById('index').style.color= "blue";
+document.getElementById('index').style.textDecoration= "underline";
 
-document.getElementById('site').style.paddingTop = "10px";
-document.getElementById('site').style.paddingLeft = "10px";
-document.getElementById('site').style.paddingRight = "10px";
-document.getElementById('site').style.paddingBottom = "10px";
+
+document.getElementById('myreader').style.backgroundColor= "#E3F1EC";
+document.getElementById('myreader').style.backgroundImage= "url(css/fundo2.png)";
+document.getElementById('myreader').style.paddingTop = "10px";
+document.getElementById('myreader').style.paddingLeft = "10px";
+document.getElementById('myreader').style.paddingRight = "10px";
+document.getElementById('myreader').style.paddingBottom = "10px";
+document.getElementById('myreader').style.textAlign= "center";
 
 document.getElementById('logo').style.width = "150px";
 document.getElementById('logo').style.height = "150px";
